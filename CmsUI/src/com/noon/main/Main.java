a5f49dcde351e2fe3fe0d32918f7670ba7491bec
SunHyongKwon <mikekwon42@gmail.com>
2022-10-06 16:50:26 +0900

    add frame

---
+++ CmsUI/src/com/noon/main/Main.java
@@ -0,0 +1,34 @@
+package com.noon.main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	private JFrame frame;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public Main() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
