ccc0dbb3c8c09e76c2f36e20036fd4fc41c9712d
gukhwa <raindrop6651@gmail.com>
2022-10-09 21:22:13 +0900

    add button change
    add button change

--- CmsUI/src/com/noon/form/parttime/ParttimeForm1.java
+++ CmsUI/src/com/noon/form/parttime/ParttimeForm1.java
@@ -9,8 +9,20 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.event.AncestorListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.event.AncestorEvent;
 
 public class ParttimeForm1 extends JPanel {
+	private JTable Inner_table;
+	private JScrollPane scrollPane;
+	
+	// -- Table Definition
+	private final DefaultTableModel Outer_Table = new DefaultTableModel(); //********  중요 ********
+	private JButton btnInsert;
+	private JScrollPane scrollPane_1;
 	private JTable table;
 
 	/**
@@ -25,30 +37,25 @@ public class ParttimeForm1 extends JPanel {
 		add(lblNewLabel);
 		
 		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.addAncestorListener(new AncestorListener() {
+			public void ancestorAdded(AncestorEvent event) {
+			}
+			public void ancestorMoved(AncestorEvent event) {
+			}
+			public void ancestorRemoved(AncestorEvent event) {
+			}
+		});
 		scrollPane.setBounds(30, 100, 418, 321);
 		add(scrollPane);
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
+		tableInit();
 		
 		JLabel lblNewLabel_1 = new JLabel("미완료 건수 : ");
 		lblNewLabel_1.setFont(new Font("굴림", Font.BOLD, 20));
-		lblNewLabel_1.setBounds(30, 443, 195, 39);
+		lblNewLabel_1.setBounds(30, 442, 195, 39);
 		add(lblNewLabel_1);
-		
-		JButton btnNewButton = new JButton("취소");
-		btnNewButton.setFont(new Font("굴림", Font.BOLD, 18));
-		btnNewButton.setBounds(198, 492, 91, 50);
-		add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("선택");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_1.setFont(new Font("굴림", Font.BOLD, 18));
-		btnNewButton_1.setBounds(331, 492, 91, 50);
-		add(btnNewButton_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("주문 리스트");
 		lblNewLabel_1_1.setFont(new Font("굴림", Font.BOLD, 40));
@@ -106,11 +113,6 @@ public class ParttimeForm1 extends JPanel {
 		lblNewLabel_1_2_2_2.setBounds(168, 443, 42, 39);
 		add(lblNewLabel_1_2_2_2);
 		
-		JButton btnNewButton_1_1 = new JButton("완료");
-		btnNewButton_1_1.setFont(new Font("굴림", Font.BOLD, 18));
-		btnNewButton_1_1.setBounds(611, 492, 91, 50);
-		add(btnNewButton_1_1);
-		
 		JLabel lblNewLabel_1_3 = new JLabel("완료 시각 : ");
 		lblNewLabel_1_3.setFont(new Font("굴림", Font.BOLD, 20));
 		lblNewLabel_1_3.setBounds(475, 443, 114, 39);
@@ -121,6 +123,68 @@ public class ParttimeForm1 extends JPanel {
 		lblNewLabel_1_2_4_1_2_1.setFont(new Font("굴림", Font.BOLD, 20));
 		lblNewLabel_1_2_4_1_2_1.setBounds(590, 442, 91, 39);
 		add(lblNewLabel_1_2_4_1_2_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\heeya\\OneDrive\\바탕 화면\\noonProject\\icon\\완료버튼.png"));
+		lblNewLabel_2.setBounds(653, 492, 115, 50);
+		add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("New label");
+		lblNewLabel_2_1.setIcon(new ImageIcon("C:\\Users\\heeya\\OneDrive\\바탕 화면\\noonProject\\icon\\선택버튼.png"));
+		lblNewLabel_2_1.setBounds(337, 492, 115, 50);
+		add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("New label");
+		lblNewLabel_2_2.setIcon(new ImageIcon("C:\\Users\\heeya\\OneDrive\\바탕 화면\\noonProject\\icon\\취소버튼.png"));
+		lblNewLabel_2_2.setBounds(207, 492, 115, 50);
+		add(lblNewLabel_2_2);
+		
+		JLabel lblNewLabel_2_3 = new JLabel("New label");
+		lblNewLabel_2_3.setIcon(new ImageIcon("C:\\Users\\heeya\\OneDrive\\바탕 화면\\noonProject\\icon\\정보수정 버튼.png"));
+		lblNewLabel_2_3.setBounds(30, 491, 115, 50);
+		add(lblNewLabel_2_3);
 
 	}
+	
+	// Method
+	
+	
+	
+	// Init the table
+			private void tableInit() {
+				Outer_Table.addColumn("순서");
+				Outer_Table.addColumn("이름");
+				Outer_Table.addColumn("전화번호");
+				Outer_Table.addColumn("관계");
+
+				Outer_Table.setColumnCount(4);
+
+				// table에 있는 데이터 지우기
+				int a = Outer_Table.getRowCount();
+				for (int i = 0; i < a; i++) {
+					Outer_Table.removeRow(0); // 열이 하나씩 당겨지기 때문에
+				}
+
+				Inner_table.setAutoResizeMode(Inner_table.AUTO_RESIZE_OFF);
+				// 순서 column
+				int vColIndex = 0;
+				TableColumn col = Inner_table.getColumnModel().getColumn(vColIndex);
+				int width = 50;
+				col.setPreferredWidth(width);
+				// 이름 column
+				vColIndex = 1;
+				col = Inner_table.getColumnModel().getColumn(vColIndex);
+				width = 100;
+				col.setPreferredWidth(width);
+				// 전화번호 column
+				vColIndex = 2;
+				col = Inner_table.getColumnModel().getColumn(vColIndex);
+				width = 150;
+				col.setPreferredWidth(width);
+				// 관계 column
+				vColIndex = 3;
+				col = Inner_table.getColumnModel().getColumn(vColIndex);
+				width = 120;
+				col.setPreferredWidth(width);
+			}
 }
