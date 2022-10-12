291888a2e87492a447b8f5ea4f037703f007c5c3
vxornjs11 <vxornjs11@naver.com>
2022-10-12 11:13:08 +0900

    Shopup

---
+++ CmsUI/src/com/noon/dao/DaoShopup.java
@@ -0,0 +1,119 @@
+package com.noon.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.noon.util.DBConnect;
+
+public class DaoShopup {
+	int shopcode;
+	String name;
+	String phone;
+	int shopregisterno;
+	int shop_shopcode;
+	String registerdate;
+	String executive_id;
+	
+	
+	
+	
+	
+	
+	
+
+	public DaoShopup(int shopcode, String name, String phone) {
+		super();
+		this.shopcode = shopcode;
+		this.name = name;
+		this.phone = phone;
+	}
+
+
+
+
+	public DaoShopup(int shop_shopcode, String executive_id) {
+		super();
+		this.shop_shopcode = shop_shopcode;
+		this.executive_id = executive_id;
+	}
+
+
+
+
+	public boolean insertShop() {
+		boolean result = false;
+		PreparedStatement ps = null;
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			Connection conn_mysql = DriverManager.getConnection(DBConnect.url_mysql, DBConnect.id_mysql,
+					DBConnect.pw_mysql); 
+			String query = "insert into shop (shopcode,name,phone) ";
+			String query1 = "values (?,?,?)";
+
+			ps = conn_mysql.prepareStatement(query + query1);
+			
+			ps.setInt(1, shopcode);
+			ps.setString(2, name);
+			ps.setString(3, phone);
+//			ps.setString(1,Panel1.id);
+//			ps.setInt(2,Panel4.detail_no);
+//			ps.setInt(3,Panel3.clickNo);
+//			ps.setInt(4, price);
+//			ps.setInt(5, amount);
+			
+			int i = ps.executeUpdate(); 
+			
+			if(i == 1) {
+				result = true;
+			}else {
+				result = false;
+			}
+
+			conn_mysql.close();
+
+		} catch (Exception e) {
+			e.printStackTrace(); 
+		}
+		return result;
+	}
+	public boolean insertShopregi() {
+		boolean result = false;
+		PreparedStatement ps = null;
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			Connection conn_mysql = DriverManager.getConnection(DBConnect.url_mysql, DBConnect.id_mysql,
+					DBConnect.pw_mysql); 
+			String query = "insert into shopregister(shop_shopcode,executive_id,registerdate) " ;
+			String query1 = "values (?,?,now())";
+
+			ps = conn_mysql.prepareStatement(query + query1);
+			
+			
+			ps.setInt(1, shop_shopcode);
+			ps.setString(2, executive_id);
+		
+			
+			int i = ps.executeUpdate(); 
+			
+			if(i == 1) {
+				result = true;
+			}else {
+				result = false;
+			}
+
+			conn_mysql.close();
+
+		} catch (Exception e) {
+			e.printStackTrace(); 
+		}
+		return result;
+	}
+	
+	
+
+}
--- CmsUI/src/com/noon/form/executive/ExecutiveForm1.java
+++ CmsUI/src/com/noon/form/executive/ExecutiveForm1.java
@@ -5,9 +5,20 @@ import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.text.NumberFormatter;
+
+import com.noon.dao.DaoManage;
+import com.noon.dao.DaoShopup;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ExecutiveForm1 extends JPanel {
 	private JTextField textField;
@@ -50,12 +61,49 @@ public class ExecutiveForm1 extends JPanel {
 		lblNewLabel_1_1_2.setBounds(133, 288, 133, 61);
 		add(lblNewLabel_1_1_2);
 		
-		textField_2 = new JTextField();
+		
+		textField_2 = new JFormattedTextField(new NumberFormatter());
 		textField_2.setColumns(10);
 		textField_2.setBounds(263, 290, 322, 45);
+		textField_2.addKeyListener(new KeyAdapter() {
+			public void KeyTyped(KeyEvent ke) {
+				if(((JFormattedTextField)ke.getSource()).getText().length() > 3)
+					ke.consume();
+			}
+		});
 		add(textField_2);
+	
+		
+//		JFormattedTextField textField_2= new JFormattedTextField(new NumberFormatter());
+//		textField_2.setColumns(10);
+//		textField_2.setBounds(263, 290, 322, 45);
+//		
+//		textField_2.addKeyListener(new KeyAdapter() {
+//			public void KeyTyped(KeyEvent ke) {
+//				if(((JFormattedTextField)ke.getSource()).getText().length() > 11)
+//					ke.consume();
+//			}
+//		});
+//		add(textField_2);
+//		
+		
 		
 		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(textField_2.getText().length() == 11 && textField_1.getText().trim().length() != 0) {
+				insertShop();
+				insertShopregi();
+				JOptionPane.showConfirmDialog(null, "지점 등록 완료");
+				}else {
+					JOptionPane.showConfirmDialog(null, "등록 양식이 틀렸습니다");
+					setVisible(false);
+				}
+				
+				
+			}
+		});
 		lblNewLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		ImageIcon icon = new ImageIcon((ExecutiveForm1.class.getResource("/com/noon/icon/Frame 8.png")));
 		Image img = icon.getImage();
@@ -65,4 +113,18 @@ public class ExecutiveForm1 extends JPanel {
 		add(lblNewLabel);
 
 	}
+	private void insertShop() { 
+		
+			DaoShopup dao = new DaoShopup(Integer.parseInt(textField.getText().trim()), textField_1.getText().trim(),textField_2.getText().trim());
+			dao.insertShop();
+
+	}
+	private void insertShopregi(){
+		
+		int num = Integer.parseInt(textField.getText().trim());
+		String ko = "mong";
+		DaoShopup dao = new DaoShopup(num,ko);
+		dao.insertShopregi();
+	}
+	
 }
